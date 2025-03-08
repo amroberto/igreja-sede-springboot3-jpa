5363a3562c1c7deae98471463d4432715e0baf2d
Alexandre M. Roberto <amroberto@gmail.com>
2025-03-08 18:12:46 -0300

    Project created

---
+++ src/main/java/com/igreja_sede/igreja_sede/IgrejaSedeApplication.java
@@ -0,0 +1,13 @@
+package com.igreja_sede.igreja_sede;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgrejaSedeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgrejaSedeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/igreja_sede/igreja_sede/IgrejaSedeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igreja_sede.igreja_sede;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgrejaSedeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
