ce52adde594d47d7143723a4e18533293ecaef97
Alexandre M. Roberto <amroberto@gmail.com>
2025-03-08 19:40:08 -0300

    Service layer, component registrtation

---
+++ src/main/java/com/igreja_sede/igreja_sede/services/UserService.java
@@ -0,0 +1,26 @@
+package com.igreja_sede.igreja_sede.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.igreja_sede.igreja_sede.entities.User;
+import com.igreja_sede.igreja_sede.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
