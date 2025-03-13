a4a496231e418104c060556db952904ca18047dd
Alexandre M. Roberto <amroberto@gmail.com>
2025-03-13 18:00:17 -0300

    Exception handling - delete

--- src/main/java/com/igreja_sede/igreja_sede/resources/exceptions/REsourceExceptionHandler.java
+++ src/main/java/com/igreja_sede/igreja_sede/resources/exceptions/REsourceExceptionHandler.java
@@ -7,12 +7,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.igreja_sede.igreja_sede.services.exceptions.DatabaseException;
 import com.igreja_sede.igreja_sede.services.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
-public class REsourceExceptionHandler {
+public class ResourceExceptionHandler {
 
 	@ExceptionHandler(ResourceNotFoundException.class)
 	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
@@ -21,4 +22,12 @@ public class REsourceExceptionHandler {
 		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
+		String error = "Database Error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}	
 }
---
+++ src/main/java/com/igreja_sede/igreja_sede/services/exceptions/DatabaseException.java
@@ -0,0 +1,10 @@
+package com.igreja_sede.igreja_sede.services.exceptions;
+
+public class DatabaseException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public DatabaseException(String msg) {
+		super(msg);
+	}
+}
